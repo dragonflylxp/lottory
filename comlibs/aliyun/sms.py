b04f115df325a58148dc19d7cdfc21b28892a6a1
lixiaopeng <dragonflylxp@gmail.com>
2020-04-17 18:50:41 +0800

    Update sms.py

--- comlibs/aliyun/sms.py
+++ comlibs/aliyun/sms.py
@@ -15,8 +15,8 @@ PRODUCT_NAME = "Dysmsapi"
 DOMAIN = "dysmsapi.aliyuncs.com"
 #
 # # ACCESS_KEY_ID/ACCESS_KEY_SECRET 根据实际申请的账号信息进行替换
-ACCESS_KEY_ID = "******"
-ACCESS_KEY_SECRET = "*******"
+ACCESS_KEY_ID = ""
+ACCESS_KEY_SECRET = ""
 
 # acs_client = None
 def set_up(config):
